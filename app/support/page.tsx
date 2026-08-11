ff5d9310a33c4add53e87b87c805bcd1e516f9fa
arowojoluabidemi7-tech <arowojoluabidemi7@gmail.com>
2026-08-11 22:01:15 +0100

    Update Telegram support link to new URL

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -27,7 +27,7 @@ export default function SupportPage() {
 
   const handleTelegramSupport = () => {
     // Open Telegram support link in a new tab
-    const telegramUrl = "https://t.me/Agentdaniel01"
+    const telegramUrl = "https://t.me/Wwkmstokf"
     window.open(telegramUrl, "_blank")
   }
 
